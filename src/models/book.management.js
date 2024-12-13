c456a954f6f73d1348b851e4b9f687c256559715
Pradumya Verma <pradumya@samyotech.com>
2024-12-13 17:57:16 +0530

    added logic for upload

--- src/models/book.management.js
+++ src/models/book.management.js
@@ -1,51 +1,54 @@
 import mongoose, { Schema } from "mongoose";
 
-const BookManagementSchema = new Schema({
-  bookName: {
-    type: String,
-    // required: true,
+const BookManagementSchema = new Schema(
+  {
+    bookName: {
+      type: String,
+      // required: true,
+    },
+    title: {
+      type: String,
+      // required: true,
+    },
+    bookIssueDate: {
+      type: Date,
+      default: Date.now(),
+    },
+    author: {
+      type: String,
+      // required: true,
+    },
+    publisherName: {
+      type: String,
+      // required: true,
+    },
+    upload_Book: {
+      type: String,
+      // required: false,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    // amount: {
+    //   type: Number,
+    //   required: true,
+    // },
+    // returnPrice: {
+    //   type: Number,
+    //   required: true,
+    // },
+    // quantity: {
+    //   type: Number,
+    //   required: true,
+    // },
+    bookDistribution: {
+      type: String,
+      // required: true,
+    },
   },
-  title: {
-    type: String,
-    // required: true,
-  },
-  bookIssueDate: {
-    type: Date,
-    default: Date.now(),
-  },
-  author: {
-    type: String,
-    // required: true,
-  },
-  publisherName: {
-    type: String,
-    // required: true,
-  },
-  upload_Book: {
-    type: String,
-    // required: false,
-  },
-  active: {
-    type: Boolean,
-    default: true,
-  },
-  // amount: {
-  //   type: Number,
-  //   required: true,
-  // },
-  // returnPrice: {
-  //   type: Number,
-  //   required: true,
-  // },
-  // quantity: {
-  //   type: Number,
-  //   required: true,
-  // },
-  bookDistribution: {
-    type: String,
-    // required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export const BookManagement = mongoose.model(
   "BookManagement",
