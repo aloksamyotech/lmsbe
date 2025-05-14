7fd78ea66e626a64957c4c564f9a1fd5c8de06e6
diksha <diksha.kushwah@samyotech.in>
2025-05-14 12:43:29 +0530

    upload folder

--- index.js
+++ index.js
@@ -5,22 +5,38 @@ import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 import { configDotenv } from "dotenv";
+import fs from "fs";
+
+configDotenv();
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+const uploadDir = path.join(__dirname, "uploads");
+
+const ensureUploadDir = () => {
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true }); 
+  }
+};
+
+ensureUploadDir();
+
+app.use("/uploads", express.static(uploadDir));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(router);
 
+
 app.get("/", (req, res) => {
-  res.send("server is running on /");
+  res.send("Server is running on /");
 });
 
 connectDb();
+
 app.listen(4300, () => {
   console.log("Server is running on port 4300");
 });
